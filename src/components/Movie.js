d49a4f51920aea1ceb3f85b7c5293c5016414e49
Vignesh <joshivignesh@users.noreply.github.com>
2021-11-02 16:43:10 +0530

    Added initial components

---
+++ src/components/Movie.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const DEFAULT_PLACEHOLDER_IMAGE = 'https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg';
+
+const Movie = ({movie}) => {
+
+}
